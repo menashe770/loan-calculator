1645a54a2b33b5e78c71e75bd075c9364a1f5a5a
Menashe <mborukhov770@gmail.com>
2021-08-02 15:37:44 -0600

    added test code

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,28 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  const values = {
+    amount: 350000,
+    years: 30,
+    rate: 3.5
+  };
+  expect(calculateMonthlyPayment(values)).toEqual(1571.66);
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  const values = {
+    amount: 0,
+    years: 30,
+    rate: 3.5
+  };
+  expect(calculateMonthlyPayment(values)).toEqual(0.00);
 });
 
-/// etc
+it("should calculate ridiculously high prices", function() {
+  const values = {
+    amount: 1000000000,
+    years: 30,
+    rate: 3.5
+  };
+  expect(calculateMonthlyPayment(values)).toEqual(4490446.88);
+});
